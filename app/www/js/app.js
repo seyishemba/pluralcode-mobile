2475e363d1abb557afde05af58ac8bc2a856dff9
oke <mail2oke@gmail.com>
2016-10-18 22:16:29 +0100

    Home tab designs

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,5 +1,25 @@
 angular.module('App', ['ionic'])
 
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+	$stateProvider.state('tabs', {
+		url: '/tabs',
+		abstract: true,
+		templateUrl: 'views/tabs/tabs.html'
+		})
+	.state('tabs.home', {
+		url: '/home',
+		views: {
+			'home-tab': {
+				templateUrl: 'views/home/home.html'
+			}
+		}
+	});
+	$urlRouterProvider.otherwise('/tabs/home');
+	$ionicConfigProvider.tabs.position('top');
+	
+	})
+ 
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
